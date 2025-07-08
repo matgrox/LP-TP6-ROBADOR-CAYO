6d6316554fc079d07e107f668e693fde7dafd023
Matias R <matiasrob.mer@gmail.com>
2025-07-08 00:01:23 -0300

    "Inicializacion de repositorio".

---
+++ src/main/java/dev/labintec/frontend/demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dev.labintec.frontend;
+
+/**
+ *
+ * @author matgr
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
